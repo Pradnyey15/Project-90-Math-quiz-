1af06ab06b7dccbc7bcb283a8529c65bfede692d
Pradnyey15 <96004377+Pradnyey15@users.noreply.github.com>
2022-02-06 21:25:26 +0530

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,10 @@
+function adduser()
+{
+    player1 = document.getElementById("player1_name").value;
+    player2 = document.getElementById("player2_name").value;
+
+    localStorage.setItem("player1_name", player1);
+    localStorage.setItem("player2_name", player2);
+
+    window.location.replace("game_page.html")
+}
